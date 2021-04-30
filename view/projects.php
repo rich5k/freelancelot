7f8e00a4a404fd89db1e8555338f0416c90d32e5
rich5k <richardanatsui@gmail.com>
2021-04-30 05:34:43 +0100

    added projects page

--- view/projects.php
+++ view/projects.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Freelancelot</title>
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <title>Find a Project of Your Interest!</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
     <!-- <link rel="stylesheet" href="./css/signUp.css"> -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
@@ -24,7 +24,7 @@
                 <a class="nav-link" href="#">Find Talent <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="./view/projects.php">Find Work </a>
+                <a class="nav-link" href="./projects.php">Find Work </a>
             </li>
             </ul>
             
@@ -61,72 +61,55 @@ _SIGNOUTITEM;
     </div>
     </nav>
         
+    <img src="../assets/workpic4.jpg" class="img-fluid" alt="Responsive image" style="
+    height: 350px;
+    width: 100%;">
     <div class="container">
+        <div class="jumbotron">
+            <h3>Browse Current Available Projects</h3>
+        
+        </div>
+        <div class="jumbotron">
+            <a href="project.php"><h5>YouTube Channel Growth</h5></a>
+            <small>Fixed price- Posted 40 minutes ago</small>
             <div class="row">
-                <div class="col-lg-6">
-                    <h1>The nation's competency builder</h1>
-                    <h5>Engage the largest network of trusted college students to unlock the 
-                    full potential of your business</h5>
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <button type="button" onclick="window.location.href='#';" class="btn btn-success btn-sm">Find Talent</button>
-                        </div>
-                        <div class="col-lg-6">
-                            <button type="button" onclick="window.location.href='./view/projects.php';" class="btn btn-outline-success btn-sm">Find Work</button>
-                        </div>
-                    
-                    </div>
+                <div class="col-lg-3">
+                    $1,000
+                    <br>
+                    Fixed Price
                 </div>
 
-                <div class="col-lg-6">
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <img src="./assets/homepic1.jpg"  class="rounded img-fluid">
-                            </div>
-                            <div class="col-lg-6">
-                            
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-6">
-                            
-                            </div>
-                            <div class="col-lg-6">
-                            <img src="./assets/homepic2.jpg"  class="rounded img-fluid">
-                            </div>
-                        </div>
+                <div class="col-lg-9">
+                    $$
+                    <br>
+                    Intermediate
                 </div>
             
             </div>
-        <h3> WHAT THEY SAY</h3>
-        <div class="row">
-            <div class="col-lg-6">
-                <img src="./assets/homepic4.jpg"  class="rounded img-fluid">
-            </div>
-            <div class="col-lg-6">
-                <p>
-                    "The student talent that I'm able to access created productivity in
-                    a way that I don't think any of us thought possible"
-                </p>
 
-                <strong>Abena Antwi</strong>
-                <h5>Director Strategic Marketing, Tullow GH</h5>
-            </div>
         </div>
-        <div class="row">
-            <div class="col-lg-6">
-                <p>
-                    "My relationship with Abena & Tullow GH has grown over the years. We've
-                    continued to work together on larger and more technical projects."
-                </p>
 
-                <strong>Steven Baidoo</strong>
-                <h5>Computer Science Student</h5>
+        <div class="jumbotron">
+            <a href="project.php"><h5>Social Media Marketing</h5></a>
+            
+            <small>Fixed price- Posted 50 minutes ago</small>
+            <div class="row">
+                <div class="col-lg-3">
+                    $500
+                    <br>
+                    Fixed Price
+                </div>
+
+                <div class="col-lg-9">
+                    $$
+                    <br>
+                    Intermediate
+                </div>
+            
             </div>
-            <div class="col-lg-6">
-                <img src="./assets/homepic5.jpg"  class="rounded img-fluid">
-            </div>
+
         </div>
+        
     </div>
 
 
@@ -139,6 +122,6 @@ _SIGNOUTITEM;
     </footer>
                         
     <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
-	<script type="text/javascript" src="./js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="../js/bootstrap.min.js"></script>
 </body>
 </html>
