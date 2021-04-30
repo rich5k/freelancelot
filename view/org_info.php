62f9b447207f86bee5702b5e4cbdb9876df98c23
rich5k <richardanatsui@gmail.com>
2021-04-30 18:04:59 +0100

    created bio and info  pages

---
+++ view/org_info.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Bio</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/project.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <!-- navbar container -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between ">
+    <div class="container">
+        <a class="navbar-brand " href="index.php"><img src="./assets/adleksLogo1.png" alt=""></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item ">
+                <a class="nav-link" href="#">Find Talent </a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="./projects.php">Find Work <span class="sr-only">(current)</span></a>
+            </li>
+            </ul>
+            
+            <ul class="navbar-nav my-2 my-lg-0 ml-auto" >
+                <li class="nav-item signIn">
+                <?php
+                //if session variable has been created, put first name and last name in navbar
+                        if(isset($_SESSION['sessionFname'])&&isset($_SESSION['sessionLname'])){
+                            printf('Akwaaba, %s %s', $_SESSION['sessionFname'], $_SESSION['sessionLname']);
+                            echo <<<_SIGNOUTITEM
+                            <button type="button" class="btn btn-success"><a id="sign-in" class="nav-link" href="./controller/logout.php">
+                                    Sign Out 
+                                <i class="fa fa-sign-out" aria-hidden="true"></i></a></button>
+                            
+_SIGNOUTITEM;
+
+                        }else{
+                            //if not, put the default navitem
+                            echo <<<_SIGNINITEM
+                            <button type="button" class="btn btn-success"><a id="sign-in" class="nav-link" href="./view/signIn.php">
+                                Sign In 
+                            <i class="fa fa-user" aria-hidden="true"></i></a></button>
+                            
+                            _SIGNINITEM;
+
+                            
+                        }
+                    ?> 
+                
+            </li>
+            
+            </ul>
+        </div>
+    </div>
+    </nav>
+        
+    <img src="../assets/workpic6.jpg" class="img-fluid" alt="Responsive image" style="
+    height: 350px;
+    width: 100%;">
+    <div class="container">
+        <div class="jumbotron">
+            <h3>Create a Profile</h3>
+            <br>
+            <br>
+            <h5>Tell Us About Your Organization</h5>
+            <form action="../controller/addCinfo.php" method="post">
+                <div class="form-group">
+                    <label for="companyInfo">Company Info:</label>
+                    <textarea type="text" class="form-control" id="companyInfo" name="companyInfo" cols="30" rows="10"></textarea>
+                    <br>
+                    <label for="clocation">Location</label>
+                    <input type="text" class="form-control" id="clocation" name="clocation" required>
+                    <br>
+                    <label for="cwebsite">Website</label>
+                    <input type="text" class="form-control" id="cwebsite" name="cwebsite" required>
+                </div>
+                
+
+                <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+                
+            </form>
+        </div>
+        
+    </div>
+
+
+    <!-- footer -->
+    <footer class="pt-5 pb-3 ">
+           <div class="credits text-center mt-2">
+                <p>Made by KAQA &copy; 2021. All rights reserved.</p>
+            </div>
+        </div>
+    </footer>
+                        
+    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="../js/bootstrap.min.js"></script>
+</body>
+</html>
