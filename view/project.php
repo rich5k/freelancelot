a6def13a68b16ef4fc359d8e0d00208eff418c05
rich5k <richardanatsui@gmail.com>
2021-04-30 16:43:26 +0100

    created project info page

--- view/project.php
+++ view/project.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Find a Project of Your Interest!</title>
+    <title>Project Name</title>
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/project.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -61,64 +61,65 @@ _SIGNOUTITEM;
     </div>
     </nav>
         
-    <img src="../assets/workpic4.jpg" class="img-fluid" alt="Responsive image" style="
+    <img src="../assets/workpic5.jpg" class="img-fluid" alt="Responsive image" style="
     height: 350px;
     width: 100%;">
     <div class="container">
+        <h3>Job Details</h3>
         <div class="jumbotron">
-            <h3>Browse Current Available Projects</h3>
+        <h5>YouTube Channel Growth</h5>
+        </div>
+        <div class="jumbotron">
+            <small>Posted 40 minutes ago</small>
+            <br>
+            <i class="fa fa-map-marker" aria-hidden="true"></i>
+            Only students in Ghanaian colleges may apply.
+            
+            <br>
+        </div>
+
+        <div class="jumbotron">
+            <p>Sales can be made by phone or by emailing. You speak and write well
+             in English and have a background in sales and preferably in travel, 
+             leisure and lifestyle sales including health products and fashion. 
+             We are not sure what to expect as far a renumeration and are open 
+             to discussion, please give us an idea of this with a proposal.</p>
+            
+            
+        </div>
+
+        <div class="jumbotron">
+            <div class="row">
+                <div class="col-lg-4">
+                    <strong><i class="fa fa-tag" aria-hidden="true"></i> $1,000</strong>
+                    <br>
+                    <small>Fixed Price</small>
+                </div> 
+                <div class="col-lg-4">
+                    <strong><i class="fa fa-briefcase" aria-hidden="true"></i>  Intermediate</strong> 
+                    <br>
+                    <small>I am willing to pay for a student who has experience
+                    and can produce value</small>
+                </div>
+                <div class="col-lg-4">
+                    <strong><i class="fa fa-map-marker" aria-hidden="true"></i> Remote Job</strong>
+                </div>
+            
+            </div>
+            <br>
+            <br>
+            <div class="row">
+                <div class="col-lg-4">
+                </div>
+                <div class="col-lg-4">
+                    <button type="button" onclick="window.location.href='proposal.php';" class="btn btn-success btn-sm">Send Proposal</button>
+                </div>
+                <div class="col-lg-4">
+                </div>
+            </div>
+        </div>
+
         
-        </div>
-        <div class="jumbotron">
-            <a href="project.php"><h5>YouTube Channel Growth</h5></a>
-            <small>Fixed price- Posted 40 minutes ago</small>
-            <div class="row">
-                <div class="col-lg-3">
-                    $1,000
-                    <br>
-                    Fixed Price
-                </div>
-
-                <div class="col-lg-9">
-                    $$
-                    <br>
-                    Intermediate
-                </div>
-            
-            </div>
-            <br>
-            <p>Sales can be made by phone or by emailing. You speak and write well
-             in English and have a background in sales and preferably in travel, 
-             leisure and lifestyle sales including health products and fashion. 
-             We are not sure what to expect as far a renumeration and are open 
-             to discussion, please give us an idea of this with a proposal.</p>
-        </div>
-
-        <div class="jumbotron">
-            <a href="project.php"><h5>Social Media Marketing</h5></a>
-            
-            <small>Fixed price- Posted 50 minutes ago</small>
-            <div class="row">
-                <div class="col-lg-3">
-                    $500
-                    <br>
-                    Fixed Price
-                </div>
-
-                <div class="col-lg-9">
-                    $$
-                    <br>
-                    Intermediate
-                </div>
-            
-            </div>
-            <br>
-            <p>Sales can be made by phone or by emailing. You speak and write well
-             in English and have a background in sales and preferably in travel, 
-             leisure and lifestyle sales including health products and fashion. 
-             We are not sure what to expect as far a renumeration and are open 
-             to discussion, please give us an idea of this with a proposal.</p>
-        </div>
         
     </div>
 
