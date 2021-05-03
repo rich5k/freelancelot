0db3bc1088d5f43276e653d24e3ff6116b2e3e7a
rich5k <richardanatsui@gmail.com>
2021-05-03 03:00:10 +0100

    modified login backend file

--- controller/login.php
+++ controller/login.php
@@ -3,7 +3,7 @@
 if(isset($_POST['submit'])){
     require '../controller/database.php';
     require_once '../models/Student.php';
-    require_once '../models/Lecturer.php';
+    require_once '../models/Organization.php';
     require_once '../models/Registry.php';
     require_once '../models/Database.php';
 
@@ -14,16 +14,15 @@ if(isset($_POST['submit'])){
     //Instantiate Student
     $student= new Student();
 
-    //Instantiate Lecturer
-    $lecturer= new Lecturer();
+    //Instantiate Organization
+    $organization= new Organization();
 
-    //Instantiate Registry
-    $registry= new Registry();
+    
 
     //if fields are empty
     if(empty($email)||empty($password)){
         echo '<script>alert("Empty Fields")</script>';
-        echo '<script>window.location.href = "../view/sign_in.php";</script>';
+        echo '<script>window.location.href = "../view/signIn.php";</script>';
        
         exit();
     }
@@ -37,7 +36,7 @@ if(isset($_POST['submit'])){
             ];
             if(!($student->getStudentEmail($studentEmail))){
                 echo '<script>alert("No user")</script>';
-                echo '<script>window.location.href = "../view/sign_up.php";</script>';
+                echo '<script>window.location.href = "../view/signUp.php";</script>';
                 exit();
             }
 
@@ -51,7 +50,7 @@ if(isset($_POST['submit'])){
                 echo $passCheck;
                 if($passCheck==false){
                     echo '<script>alert("Wrong Password")</script>';
-                    echo '<script>window.location.href = "../view/sign_in.php";</script>';
+                    echo '<script>window.location.href = "../view/signIn.php";</script>';
                     
                     exit();
                 }elseif($passCheck==true){
@@ -62,54 +61,11 @@ if(isset($_POST['submit'])){
                     $_SESSION['sessionFname']=$stud->fname;
                     $_SESSION['sessionLname']=$stud->lname;
                     echo '<script>alert("Well Done. Logged in successfully")</script>';
-                    echo '<script>window.location.href = "../view/student_dashboard.php";</script>';
+                    echo '<script>window.location.href = "../view/stud_dashboard.php";</script>';
                     
                 }else{
                     echo '<script>alert("Wrong Password")</script>';
-                    echo '<script>window.location.href = "../view/sign_in.php";</script>';
-                
-                }
-            }
-
-        }
-        elseif($selected_category=="lecturer"){
-
-            //lecturer Email
-            $lecturerEmail=[
-                'email'=> $email
-            ];
-            if(!($lecturer->getLecturerEmail($lecturerEmail))){
-                echo '<script>alert("No user")</script>';
-                echo '<script>window.location.href = "../view/sign_up.php";</script>';
-                exit();
-            }
-
-            else{
-            
-                //getting lecturer details
-                $lect= $lecturer->getLecturerDetails($lecturerEmail);
-                
-                //verfiying password
-                $passCheck=password_verify($password, $lect->password);
-                echo $passCheck;
-                if($passCheck==false){
-                    echo '<script>alert("Wrong Password")</script>';
-                    echo '<script>window.location.href = "../view/sign_in.php";</script>';
-                    
-                    exit();
-                }elseif($passCheck==true){
-                    session_start();
-                    //creating session variables
-                    $_SESSION['sessionId']=$lect->lecturerID;
-                    $_SESSION['sessionEmail']=$lect->email;
-                    $_SESSION['sessionFname']=$lect->fname;
-                    $_SESSION['sessionLname']=$lect->lname;
-                    echo '<script>alert("Well Done. Logged in successfully")</script>';
-                    echo '<script>window.location.href = "../view/lecturer_dashboard.php";</script>';
-                    
-                }else{
-                    echo '<script>alert("Wrong Password")</script>';
-                    echo '<script>window.location.href = "../view/sign_in.php";</script>';
+                    echo '<script>window.location.href = "../view/signIn.php";</script>';
                 
                 }
             }
@@ -117,47 +73,48 @@ if(isset($_POST['submit'])){
         }
         else{
 
-            //registry Email
-            $registryEmail=[
+            //Organization Email
+            $OrganizationEmail=[
                 'email'=> $email
             ];
-            if(!($registry->getRegistryEmail($registryEmail))){
+            if(!($Organization->getOrganizationEmail($OrganizationEmail))){
                 echo '<script>alert("No user")</script>';
-                echo '<script>window.location.href = "../view/sign_up.php";</script>';
+                echo '<script>window.location.href = "../view/signUp.php";</script>';
                 exit();
             }
 
             else{
             
-                //getting registry details
-                $reg= $registry->getRegistryDetails($registryEmail);
+                //getting Organization details
+                $org= $Organization->getOrganizationDetails($OrganizationEmail);
                 
                 //verfiying password
-                $passCheck=password_verify($password, $reg->password);
+                $passCheck=password_verify($password, $org->password);
                 echo $passCheck;
                 if($passCheck==false){
                     echo '<script>alert("Wrong Password")</script>';
-                    echo '<script>window.location.href = "../view/sign_in.php";</script>';
+                    echo '<script>window.location.href = "../view/signIn.php";</script>';
                     
                     exit();
                 }elseif($passCheck==true){
                     session_start();
                     //creating session variables
-                    $_SESSION['sessionId']=$reg->registryID;
-                    $_SESSION['sessionEmail']=$reg->email;
-                    $_SESSION['sessionFname']=$reg->fname;
-                    $_SESSION['sessionLname']=$reg->lname;
+                    $_SESSION['sessionId']=$org->organID;
+                    $_SESSION['sessionEmail']=$org->email;
+                    $_SESSION['sessionFname']=$org->cname;
+                    
                     echo '<script>alert("Well Done. Logged in successfully")</script>';
-                    echo '<script>window.location.href = "../view/reg_welcome.php";</script>';
+                    echo '<script>window.location.href = "../view/org_dashboard.php";</script>';
                     
                 }else{
                     echo '<script>alert("Wrong Password")</script>';
-                    echo '<script>window.location.href = "../view/sign_in.php";</script>';
+                    echo '<script>window.location.href = "../view/signIn.php";</script>';
                 
                 }
             }
 
         }
+        
         
     }
     
