738307022d06bbc188bbe06587a1b721c36aacff
rich5k <richardanatsui@gmail.com>
2021-05-04 02:51:08 +0100

    added ajax to radio buttons on sign up page

--- controller/register.php
+++ controller/register.php
@@ -14,149 +14,135 @@ if(isset($_POST['submit'])){
 
     
 
-    $fname= $_POST['fname'];
-    $lname= $_POST['lname'];
+    
     $email=$_POST['email'];
     $password=$_POST['pswd'];
     $confirmPass=$_POST['confirmPswd'];
     $selected_category= $_POST['category'];
-
     
     
-
-    //if fields are empty
-    if (empty($fname) ||empty($lname) || empty($email) || empty($password) || empty($confirmPass)){
-        echo '<script>alert("Some fields are empty)</script>';
-        echo '<script>window.location.href = "../view/signUp.php";</script>';
-        exit();
-    }elseif(!preg_match("/^[A-Za-z_]{3,}@[A-Za-z]{3,}[.]{1}[A-Za-z]{2,6}$/",$email) && !preg_match("/^[A-Za-z_]{3,}@[A-Za-z]{3,}[.]{1}[A-Za-z]{2,6}[.]{1}[A-Za-z]{2,6}$/",$email)){
-        echo '<script>alert("Invalid email")</script>';
-        echo '<script>window.location.href = "../view/signUp.php";</script>';
-        exit();
-    }elseif($password !== $confirmPass){
-        echo '<script>alert("Passwords do not match")</script>';
-        echo '<script>window.location.href = "../view/signUp.php";</script>';
-        exit();
-    }else{
-        
-        if($selected_category=="student"){
-
-            //Student Email
-            $studentEmail=[
-                'email'=> $email
-            ];
-            if($student->getStudentEmail($studentEmail)){
-                echo '<script>alert("Username Taken")</script>';
-                echo '<script>window.location.href = "../view/signUp.php";</script>';
-                exit();
-            }
-
-            else{
+    if($selected_category=="student"){
+        $fname= $_POST['fname'];
+    $lname= $_POST['lname'];
+        //if fields are empty
+        if (empty($fname) ||empty($lname) || empty($email) || empty($password) || empty($confirmPass)){
+            echo '<script>alert("Some fields are empty)</script>';
+            echo '<script>window.location.href = "../view/signUp.php";</script>';
+            exit();
+        }elseif(!preg_match("/^[A-Za-z_]{3,}@[A-Za-z]{3,}[.]{1}[A-Za-z]{2,6}$/",$email) && !preg_match("/^[A-Za-z_]{3,}@[A-Za-z]{3,}[.]{1}[A-Za-z]{2,6}[.]{1}[A-Za-z]{2,6}$/",$email)){
+            echo '<script>alert("Invalid email")</script>';
+            echo '<script>window.location.href = "../view/signUp.php";</script>';
+            exit();
+        }elseif($password !== $confirmPass){
+            echo '<script>alert("Passwords do not match")</script>';
+            echo '<script>window.location.href = "../view/signUp.php";</script>';
+            exit();
+        }else{
             
-                $hashedPass= password_hash($password, PASSWORD_DEFAULT);
+            if($selected_category=="student"){
     
-                //Student Data
-                $StudentData =[
-                    'fname'=> $fname,
-                    'lname'=> $lname,
-                    'email'=> $email,
-                    'password'=> $hashedPass
+                //Student Email
+                $studentEmail=[
+                    'email'=> $email
                 ];
-                
-                //Add Student To Do
-                if($student->addStudent($StudentData)){
-                    echo '<script>alert("Well Done. You have been registered successfully")</script>';
-                    echo '<script>window.location.href = "../view/signIn.php";</script>';
+                if($student->getStudentEmail($studentEmail)){
+                    echo '<script>alert("Username Taken")</script>';
+                    echo '<script>window.location.href = "../view/signUp.php";</script>';
                     exit();
+                }
     
-                }
                 else{
-                    header("Location: ../view/signUp.php?error=sqlerror1");
-                    exit();
+                
+                    $hashedPass= password_hash($password, PASSWORD_DEFAULT);
+        
+                    //Student Data
+                    $StudentData =[
+                        'fname'=> $fname,
+                        'lname'=> $lname,
+                        'email'=> $email,
+                        'password'=> $hashedPass
+                    ];
+                    
+                    //Add Student To Do
+                    if($student->addStudent($StudentData)){
+                        echo '<script>alert("Well Done. You have been registered successfully")</script>';
+                        echo '<script>window.location.href = "../view/signIn.php";</script>';
+                        exit();
+        
+                    }
+                    else{
+                        header("Location: ../view/signUp.php?error=sqlerror1");
+                        exit();
+                    }
                 }
+    
             }
-
+    
+            
+    
+            
+            
+            
         }
 
-        elseif($selected_category=="Organization"){
-
-            //Organization Email
-            $OrganizationEmail=[
-                'email'=> $email
-            ];
-            if($organization->getOrganizationEmail($OrganizationEmail)){
-                echo '<script>alert("Username Taken")</script>';
-                echo '<script>window.location.href = "../view/signUp.php";</script>';
-                exit();
-            }
-
-            else{
-            
-                $hashedPass= password_hash($password, PASSWORD_DEFAULT);
-    
-                //Organization Data
-                $OrganizationData =[
-                    'cname'=> $cname,
-                    'email'=> $email,
-                    'password'=> $hashedPass
-                ];
-                
-                //Add Organization To Do
-                if($organization->addOrganization($OrganizationData)){
-                    echo '<script>alert("Well Done. You have been registered successfully")</script>';
-                    echo '<script>window.location.href = "../view/signIn.php";</script>';
-                    exit();
-    
-                }
-                else{
-                    header("Location: ../view/signUp.php?error=sqlerror1");
-                    exit();
-                }
-            }
-
-        }
-
-        else{
-
-            //registry Email
-            $registryEmail=[
-                'email'=> $email
-            ];
-            if($registry->getRegistryEmail($registryEmail)){
-                echo '<script>alert("Username Taken")</script>';
-                echo '<script>window.location.href = "../view/signUp.php";</script>';
-                exit();
-            }
-
-            else{
-            
-                $hashedPass= password_hash($password, PASSWORD_DEFAULT);
-    
-                //registry Data
-                $registryData =[
-                    'fname'=> $fname,
-                    'lname'=> $lname,
-                    'email'=> $email,
-                    'password'=> $hashedPass
-                ];
-                
-                //Add registry To Do
-                if($registry->addRegistry($registryData)){
-                    echo '<script>alert("Well Done. You have been registered successfully")</script>';
-                    echo '<script>window.location.href = "../view/signIn.php";</script>';
-                    exit();
-    
-                }
-                else{
-                    header("Location: ../view/signUp.php?error=sqlerror1");
-                    exit();
-                }
-            }
-
-        }
-        
-        
     }
+    else{
+        $cname = $_POST['cname'];
+        //if fields are empty
+        if (empty($cname) || empty($email) || empty($password) || empty($confirmPass)){
+            echo '<script>alert("Some fields are empty)</script>';
+            echo '<script>window.location.href = "../view/signUp.php";</script>';
+            exit();
+        }elseif(!preg_match("/^[A-Za-z_]{3,}@[A-Za-z]{3,}[.]{1}[A-Za-z]{2,6}$/",$email) && !preg_match("/^[A-Za-z_]{3,}@[A-Za-z]{3,}[.]{1}[A-Za-z]{2,6}[.]{1}[A-Za-z]{2,6}$/",$email)){
+            echo '<script>alert("Invalid email")</script>';
+            echo '<script>window.location.href = "../view/signUp.php";</script>';
+            exit();
+        }elseif($password !== $confirmPass){
+            echo '<script>alert("Passwords do not match")</script>';
+            echo '<script>window.location.href = "../view/signUp.php";</script>';
+            exit();
+        }
+        else{
+            if($selected_category=="org"){
+    
+                //Organization Email
+                $OrganizationEmail=[
+                    'email'=> $email
+                ];
+                if($organization->getOrganizationEmail($OrganizationEmail)){
+                    echo '<script>alert("Username Taken")</script>';
+                    echo '<script>window.location.href = "../view/signUp.php";</script>';
+                    exit();
+                }
+    
+                else{
+                
+                    $hashedPass= password_hash($password, PASSWORD_DEFAULT);
+        
+                    //Organization Data
+                    $OrganizationData =[
+                        'cname'=> $cname,
+                        'email'=> $email,
+                        'password'=> $hashedPass
+                    ];
+                    
+                    //Add Organization To Do
+                    if($organization->addOrganization($OrganizationData)){
+                        echo '<script>alert("Well Done. You have been registered successfully")</script>';
+                        echo '<script>window.location.href = "../view/signIn.php";</script>';
+                        exit();
+        
+                    }
+                    else{
+                        header("Location: ../view/signUp.php?error=sqlerror1");
+                        exit();
+                    }
+                }
+    
+            }
+        }
+    }
+
     
 }
 
