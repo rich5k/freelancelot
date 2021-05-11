d7f3b6b68cc5cae6f6c37d4fda69f092f836c7a1
joshuaQuart <43918812+joshuaQuart@users.noreply.github.com>
2021-05-11 16:24:45 +0000

    added token and database name for project

--- joshua/recomm.js
+++ joshua/recomm.js
@@ -1,11 +1,13 @@
 <script src="https://cdn.jsdelivr.net/gh/recombee/js-api-client/dist/recombee-api-client.min.js"></script>
-    
+    var db = "freelancelot-db"
+    var token = "1zu83UTbkZBr76ONrM4TvmFIal4n3jtUJAMdUdf6z6tCKiXTc5LGUFZcSU5lsxZP";
+
     // Record opening and viewing student's profile
-    $(".check-out-student").click(function(){
+    $(".btn-check-out-student").click(function(){
         var student_id = $(this).parent().attr("id");
         var org_id = sessionStorage.getItem("sessionId");
 
-        var client = new recombee.ApiClient('myDb', publicToken);
+        var client = new recombee.ApiClient(db, token);
 
         client.send(new recombee.AddDetailView(org_id, student_id),
             (err, response) => {
@@ -15,11 +17,11 @@
     });
 
     // Record hiring
-    $(".hire_student").click(function(){
+    $(".btn-hire-student").click(function(){
         var student_id = $(this).parent().attr("id");
         var org_id = sessionStorage.getItem("sessionId");
 
-        var client = new recombee.ApiClient('myDb', publicToken);
+        var client = new recombee.ApiClient(db, token);
         client.send(new recombee.AddPurchase(org_id, student_id),
             (err, response) => {
                 console.log(response);
